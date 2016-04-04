e39c0cfd28daa586e41983187380d8495b129d13
ivahaev <ivahaev@gmail.com>
2016-04-04 17:36:33 +0500

    More tests

--- timer_test.go
+++ timer_test.go
@@ -5,14 +5,74 @@ import (
 	"time"
 )
 
-func TestStartStop(t *testing.T) {
+func TestStart(t *testing.T) {
 	var now = time.Now()
 	var timer = AfterFunc(time.Second, func() {
-		println(time.Now().Sub(now))
+		if time.Now().Sub(now) < time.Second {
+			t.Fatal("Early func")
+		}
+	})
+	timer.Start()
+	time.Sleep(time.Second * 2)
+}
+
+func TestStop(t *testing.T) {
+	var timer = AfterFunc(time.Second, func() {
+		t.Fatal("Stopping is not working")
+	})
+	timer.Start()
+	timer.Stop()
+	time.Sleep(time.Second * 2)
+}
+
+func TestPause(t *testing.T) {
+	var now = time.Now()
+	var timer = AfterFunc(time.Second, func() {
 		if time.Now().Sub(now) < time.Second*2 {
 			t.Fatal("Early func")
 		}
 	})
+	timer.Start()
+	time.Sleep(time.Microsecond * 500)
+	timer.Pause()
+	time.Sleep(time.Second)
+	timer.Start()
+	time.Sleep(time.Second)
+}
+
+func TestChanStart(t *testing.T) {
+	var now = time.Now()
+	var timer = NewTimer(time.Second)
+	go func() {
+		<-timer.C
+		if time.Now().Sub(now) < time.Second {
+			t.Fatal("Early func")
+		}
+	}()
+	timer.Start()
+	time.Sleep(time.Second * 2)
+}
+
+func TestChanStop(t *testing.T) {
+	var timer = NewTimer(time.Second)
+	go func() {
+		<-timer.C
+		t.Fatal("Stopping is not working")
+	}()
+	timer.Start()
+	timer.Stop()
+	time.Sleep(time.Second * 2)
+}
+
+func TestChanPause(t *testing.T) {
+	var now = time.Now()
+	var timer = NewTimer(time.Second)
+	go func() {
+		<-timer.C
+		if time.Now().Sub(now) < time.Second*2 {
+			t.Fatal("Early func")
+		}
+	}()
 	timer.Start()
 	time.Sleep(time.Microsecond * 500)
 	timer.Pause()
